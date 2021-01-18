a673534ffa603d22ab0864695b462cdaabaf7d96
wangzhenjun3 <wangzhenjun3@xdf.cn>
2021-01-18 15:09:38 +0800

    update

---
+++ src/main/java/study/sharding/springboot/shardingjdbc/dao/orderbase/OrderBaseMapper.java
@@ -0,0 +1,7 @@
+package study.sharding.springboot.shardingjdbc.dao.orderbase;
+
+import study.sharding.springboot.shardingjdbc.support.db.BaseMapper;
+
+public interface OrderBaseMapper extends BaseMapper<Long, OrderBaseEO> {
+
+}
--- src/main/java/study/sharding/springboot/shardingjdbc/dao/useradmin/UserAdminMapper.java
+++
@@ -1,8 +0,0 @@
-package study.sharding.springboot.shardingjdbc.dao.useradmin;
-
-import study.sharding.springboot.shardingjdbc.support.db.BaseMapper;
-
-public interface UserAdminMapper extends BaseMapper<Long, UserAdminEO> {
-
-    UserAdminEO getByUserId(Long uaUserId);
-}
